0886d6f27b3f80e9adf3fbae2575e6850ee39f90
NicolasPetiot <nicolaspetiot2710@hotmail.fr>
2026-06-24 22:08:17 +0200

    core.py tested and works as expected!

--- core.py
+++ core.py
@@ -1,42 +1,67 @@
+import argparse
 import json
 from pathlib import Path
 
+import pandas as pd
 import requests
+from requests import Response
 from requests.auth import HTTPBasicAuth
 
-PHONE = ""
-MSG = "Coucou depuis mon script Python !"
+
+def main(args):
+    sender = SMSSender(args.device)
+
+    db = pd.read_csv(args.messages)
+    for _, s in db.iterrows():
+        phone = s.phone.strip("'")
+        msg = s.message
+        _ = sender.send_sms(phone=phone, msg=msg)
 
 
-def main():
-    # Load device
-    device = Path("device.json")
-    device = json.loads(device.read_text())
+class SMSSender:
+    def __init__(self, device: Path, timeout=10) -> None:
+        """ """
+        if not device.exists():
+            raise FileNotFoundError(device)
 
-    # Create request in expected format:
-    header = {"Content-Type": "application/json"}
-    payload = {
-        "textMessage": {
-            "text": MSG,
-        },
-        "phoneNumbers": [PHONE],
-    }
+        d: dict = json.loads(device.read_text())
+        self.local_adress = self.__get_record(d, "local_adress")
+        self.username = self.__get_record(d, "username")
+        self.password = self.__get_record(d, "password")
+        self.header = {"Content-Type": "application/json"}
+        self.timeout = timeout
+        self.auth = HTTPBasicAuth(username=self.username, password=self.password)
 
-    # Actually send the request!
-    r = requests.post(
-        f"http://{device.get('local_adress')}/message",
-        json=payload,
-        headers=header,
-        auth=HTTPBasicAuth(
-            username=device.get("username"), password=device.get("password")
-        ),
-        timeout=10,
-    )
+    def send_sms(self, phone: str, msg: str) -> Response:
+        url = f"http://{self.local_adress}/message"
+        payload = {
+            "textMessage": {
+                "text": msg,
+            },
+            "phoneNumbers": [phone],
+        }
+        r = requests.post(
+            url, json=payload, headers=self.header, auth=self.auth, timeout=self.timeout
+        )
+        return r
 
-    # Display the status:
-    print(r.status_code)
-    print(r.text)
+    @staticmethod
+    def __get_record(d: dict, key: str) -> str:
+        val = d.get(key)
+        if val is not None and val != "":
+            return val
+
+        raise ValueError(f"The '{key}' record is missing from device file")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--messages",
+        type=Path,
+        help="Fichier CSV qui contient à la fois les messages et les numéros visés.",
+    )
+    parser.add_argument("--device", type=Path, default=Path("device.json"))
+    args = parser.parse_args()
+    main(args)
